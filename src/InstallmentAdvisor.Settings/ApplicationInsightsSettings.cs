229df4ca5632e73b7654a64f91508b7996d7ccac
Remco Brosky <remco.brosky@microsoft.com>
2025-07-02 02:28:58 +0200

    Observability Part 1

---
+++ src/InstallmentAdvisor.Settings/ApplicationInsightsSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace InstallmentAdvisor.Settings;
+
+public class ApplicationInsightsSettings : Base64SerializableSettings<AgentsSettings>
+{
+    public const string Key = "ApplicationInsights";
+
+    public string ConnectionString { get; set; } = "";
+}
