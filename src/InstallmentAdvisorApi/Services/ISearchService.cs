4858629315935c407a8beb08837d1a931736d5bf
Remco Eissing <remco.eissing@live.com>
2025-07-03 16:39:14 +0200

    Added Search MCP tool

---
+++ src/InstallmentAdvisorApi/Services/ISearchService.cs
@@ -0,0 +1,11 @@
+﻿using InstallmentAdvisor.DataApi.Models;
+
+namespace InstallmentAdvisor.DataApi.Services;
+
+public interface ISearchService
+{
+    public Task<string> SearchAsync(string query);
+
+    public Task<List<SearchDocumentModel>> SearchVectorAsync(float[] queryVector);
+
+}
